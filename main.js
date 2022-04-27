345c48f3cd343e7db699620363f28b9eb632799b
Paweł Bokszczanin <bokszczanin.p@gmail.com>
2022-04-27 14:50:59 +0200

    computed properties

--- main.js
+++ main.js
@@ -3,13 +3,14 @@ const app = Vue.createApp({
         return {
             cart:0,
             product: 'Socks',
+            onSale: true,
             brand: 'Vue Mastery',
-            image: './assets/images/socks_blue.jpg',
-            inStock: false,
+            selectedVariant: 0,
+            // inStock: false,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
             ]
         }
     },
@@ -17,8 +18,25 @@ const app = Vue.createApp({
         addToCart() {
             this.cart += 1
         },
-        updateImage(variantImage) {
-            this.image = variantImage
+        updateVariant(index) {
+            this.selectedVariant = index
+            console.log(index);
         }
+    },
+    computed: {
+      title() {
+        return this.brand + ' ' + this.product
+      },
+      image() {
+        return this.variants[this.selectedVariant].image
+      },
+      inStock() {
+        return this.variants[this.selectedVariant].quantity
+      },
+      saleMessage() {
+        if(this.onSale) {
+          return this.brand + ' ' + this.product + ' ' + 'in on Sale'        }
+      } 
+      
     }
 })
